c737fb257387aed4e5d33a3db350d940cec7e111
Archit Sachdeva <archit.sachdeva007@gmail.com>
2025-01-16 02:03:56 +0000

    render deployment linked with github actions now fingers crossed

--- main.py
+++ main.py
@@ -694,4 +694,5 @@ def get_response():
 # Entry Point
 # ----------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
